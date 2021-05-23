4e59b8688e5ec8cefc0c34cfda319503215f67c2
Sooraj S Nair <nairsooraj2000@gmail.com>
2021-05-24 00:03:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'sanskrit_stemmer',         # How you named your package folder (MyLib)
   packages = ['sanskrit_stemmer'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A rule-based stemmer for Sanskrit Verbs and Nouns',   # Give a short description about your library
   author = 'Sooraj S Nair',                   # Type in your name
   author_email = 'soorajsnair@am.students.amrita.edu',      # Type in your E-Mail
   url = 'https://github.com/sooraj-nair/sanskrit_stemmer',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sooraj-nair/sanskrit_stemmer/archive/refs/tags/v_01.tar.gz',   
+  download_url = 'https://github.com/sooraj-nair/sanskrit_stemmer/archive/refs/tags/v_02.tar.gz',   
   keywords = ['Sanskrit', 'Stemmmer'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'devatrans',
